b057ee5dca47b3a40c26f22d9c80a3bf8c51c36e
ElTruquini <d_olaya@hotmail.com>
2016-01-31 14:06:53 -0800

    Methods completed

--- MedListRefBased.java
+++ MedListRefBased.java
@@ -30,7 +30,7 @@ public class MedListRefBased implements List<Medication> {
 		listCount=0;
 
 	}
-	//Internal method to print content of list
+	/*Internal method to print content of list
 	public void print (){
 		System.out.println ("-------------:::::PRINTING LIST:::::----------");
 		curr = head.next;
@@ -39,9 +39,8 @@ public class MedListRefBased implements List<Medication> {
 			curr = curr.next;
 		}
 		System.out.println ("----------:::::END PRINTING LIST:::::---------");
-
 	}
-
+	*/
 	public void add(Medication k,int index) {
 		MedicationNode newNode = new MedicationNode (k);
 		
@@ -60,56 +59,96 @@ public class MedListRefBased implements List<Medication> {
 		newNode.prev = curr.prev;
 		curr.prev = newNode;
 		newNode.prev.next = newNode;// How does this work??????
-
 		listCount ++;
-		System.out.println ("\n" );
-		print ();
+	}
 
-/*Traverse with index tester
+	public void remove(int index) {
+		
+		if (index <0 || index >listCount){
+			throw new ListIndexOutOfBoundsException("The index "+index+" is out of bounds.");
+		}
 		curr = head.next;
-		i = 0;
-		System.out.println ("head is at: " + head.item + "-" + head + " ********************* " );
+		int i = 0;
+		//System.out.println ("head is at: " + head.item + "-" + head + " ********************* " );
 		while ((curr != head) && (i < index)) {
 			curr = curr.next;
 			i++;
 		}
-		System.out.println ("curr is at: " + curr.item + " - " + curr+ ", at index - " + index + " ********************* " );
-*/
-
-
-	}
-
-
-	public void remove(int index) {
+		//System.out.println ("curr is at: " + curr.item + "- index" + index + " REMOVE********************* " );
+		curr.prev.next = curr.next;
+		curr.next.prev = curr.prev;
+		listCount --;
+		//System.out.println ("\n" );
+		//print ();
 	}
 
 	public Medication get(int index) {
-	return null;
+		if (index <0 || index >listCount){
+			throw new ListIndexOutOfBoundsException("The index "+index+" is out of bounds.");
+		}
+		curr = head.next;
+		int i = 0;
+		//System.out.println ("head is at: " + head.item + "-" + head + " ********************* " );
+		while ((curr != head) && (i < index)) {
+			curr = curr.next;
+			i++;
+		}
+		return curr.item;
 	}
 
 	public boolean isEmpty() {
-	return true;
-	}
+			if (listCount != 0){
+				return false;
+			}
+		return true;
+		}
 
-	public int size() {
-	return 3;
+		public int size() {
+		return listCount;
 	}
 
 	public void removeAll() {
+		head = dummy;
+		head.next = dummy; 
+		head.prev = dummy;
+		listCount = 0;
 	}
 
 	public int find(Medication item) {
-	return -4;
+		int i = 0;
+		curr = head.next;
+		while (curr != head) {
+		//System.out.println ("curr is at: " + curr.item + "- index" + i + " FIND********************* " );
+		//System.out.println ("" + curr.item.equals(item) + curr.item + " at i: " + i);
+			if (curr.item.equals(item)){
+				//System.out.println ("curr: " + curr.item + "- vs. " + item + " " + curr.item.equals(item) + " FIND*********" );
+				return i;
+			}
+			curr = curr.next;
+			i++;
+		}
+		return -1;
 	}
 
 	public void remove(Medication item) {
+		do {
+			if (find (item) != -1){
+				remove (find(item));
+			} 
+		}while (find (item) != -1);	
 	}
-/*
+
 	public String toString() {
-		//String print = ""+ item.getName + " : " + item.getDosage + " mg" ;
-	return "x";
+		String print = "---===Begining of List===---\n";
+		curr = head.next;
+		while (curr != head){
+			print = print + "" + curr.item.getName() + " : " + curr.item.getDosage() + " mg" + "\n";
+			curr = curr.next;
+		}
+		print += "---===End of List===---";
+		return print;
 	}
-*/
+
 	public static void main(String[] args) {
 		// Creating Medication
 		Medication m1 = new Medication ("0- DMT", 200);
@@ -117,14 +156,14 @@ public class MedListRefBased implements List<Medication> {
 		Medication m3 = new Medication ("2- Psilocybin", 450);
 		Medication m4 = new Medication ("3- Modafinil", 75);
 		Medication m5 = new Medication ("4- 2CB", 100);
+		Medication m6 = new Medication ("5- Cannabinoids", 200);
+
 
 		//Creating nodes only items
 		MedicationNode dan1 = new MedicationNode (m1);
 		MedicationNode dan2 = new MedicationNode (m2);
 		System.out.println ("\n" );
-		/*
 
-		*/
 		//Constructor
 		MedListRefBased list = null;
 		try {
@@ -135,13 +174,21 @@ public class MedListRefBased implements List<Medication> {
 		return;
 		}
 
+/*Testing add method
+		System.out.println ("Must be true: " + list.isEmpty());
+		System.out.println ("Must be 0: " + list.size());
+*/
 		list.add (m1,0); //dmt
 		list.add (m2,1); //lsd
 		list.add (m3,2); //psi
 		list.add (m4,3); //mod
 		list.add (m5,4); //2cb
+		list.add (m1,3); //dmt
+		list.add (m1,5); //dmt
 
-		/*Add method tester
+		//list.print ();
+/* 
+		//Add method tester
 		list.add (m1,0); //dmt
 		list.add (m2,1); //lsd
 		list.add (m3,2); //psi
@@ -162,7 +209,54 @@ public class MedListRefBased implements List<Medication> {
 		list.add (m4,2); //3.mod
 		list.add (m5,0); //4.2cb
 		System.out.println ("Expected result: 4,0,2,3,0");
-		*/
-	}		
-}
+		
+		//Testing remove method		
+		list.remove(1);
+		list.remove(0);
+		list.remove(2);
+		
+		//Testing get method
+		System.out.println (list.get(0));	
+		System.out.println (list.get(1));	
+		System.out.println (list.get(2));	
+		System.out.println (list.get(3));	
+		System.out.println (list.get(4));	
+		System.out.println (list.get(10));	
 
+		System.out.println ("Must be false: " + list.isEmpty());
+		System.out.println ("Must be 5: " + list.size());
+
+		//Testing Remove all
+		list.removeAll();
+		list.print();
+		
+		list.removeAll();
+		list.add (m1,0); //dmt
+		list.add (m2,1); //lsd
+		list.print();
+		
+		//list.find(m4);
+		System.out.println (list.find(m4)); //must return 3
+		//list.find(m6);
+		System.out.println (list.find(m6)); //must return -1
+		
+		list.remove(m1);
+*/
+		//list.remove (m1);	
+		//list.remove (3);		
+		//list.remove (5);		
+	
+		System.out.println(list);
+
+		System.out.print("Checking the removeAll method: ");
+		list.removeAll();
+		System.out.println(""+!list.isEmpty());
+
+		if (!list.isEmpty()) {
+
+			System.out.println("FAILURE");
+			return;
+		}
+
+	}
+}
